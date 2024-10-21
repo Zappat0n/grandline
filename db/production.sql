0f725719aaa405614f886f4a9cadcc9558662af4
Angel Luis Barros Pazos <albarros1973@gmail.com>
2024-10-21 02:14:27 +0200

    First

---
+++ db/production.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `grandline_production`
+CREATE DATABASE IF NOT EXISTS `grandline_production_cache`
+CREATE DATABASE IF NOT EXISTS `grandline_production_queue`
+CREATE DATABASE IF NOT EXISTS `grandline_production_cable`
