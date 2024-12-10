f15d0b322d8885fa0372814104f82a5587227f19
Angel Luis Barros Pazos <albarros1973@gmail.com>
2024-12-10 22:20:35 +0100

    Improve streaming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user_cookie
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def set_user_cookie
+    cookies[:user_id] = current_user.id if user_signed_in?
   end
 end
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,10 +2,10 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    message = Message.find(message.id)
     ActionCable.server.broadcast(
       message.room_name,
       {
+        user_id: message.user_id,
         html: ApplicationController.new.render_to_string(partial: "messages/message", locals: { message: message, user: message.user })
       }
     )
