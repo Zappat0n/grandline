991a2969b516b2f9587bba48437d3609c059a0a1
Angel Luis Barros Pazos <albarros1973@gmail.com>
2024-12-22 18:51:07 +0100

    Fix importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "lodash", to: "lodash.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/controllers/helpers", under: "helpers", to: "helpers"
