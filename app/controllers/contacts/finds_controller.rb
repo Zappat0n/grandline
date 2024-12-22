59386157565e3a7052e73b0c2d607ea4d689e5b4
Angel Luis Barros Pazos <albarros1973@gmail.com>
2024-12-22 17:55:46 +0100

    Implement add contact

--- app/controllers/contacts/finds_controller.rb
+++ app/controllers/contacts/finds_controller.rb
@@ -1,6 +1,21 @@
-class Contacts::FindController < ApplicationController
+class Contacts::FindsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     contact = User.find_by(username: params[:username])
-    byebu
+
+    render json: {
+      html: html_for(contact)
+    }.to_json
+  end
+
+  private
+
+  def html_for(user)
+    if user
+      render_to_string(partial: "contacts/add_contact", formats: [:html], locals: { contact: user })
+    else
+      render_to_string(partial: "contacts/no_contact", formats: [:html])
+    end
   end
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     contact = User.find(params[:contact_id])
